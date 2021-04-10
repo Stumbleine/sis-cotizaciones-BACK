a2f392f601e55ac8403c55ca666d878b29d9d56e
jh3ys0n <55772934+jh3ys0n@users.noreply.github.com>
2021-04-10 11:43:11 -0400

    primer commit con STS

---
+++ QuotationApp/src/main/java/com/umss/dev/QuotationApp.java
@@ -0,0 +1,13 @@
+package com.umss.dev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuotationApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuotationApp.class, args);
+	}
+
+}
