c87deeca9f6158c0befd3797cf0acb8cddf8af5f
janeth <jl715643@gmail.com>
2021-05-16 15:57:43 -0400

    Get mejorado para mostrar todas las cotizaciones de un solicitud de pedido

--- src/main/java/com/umss/dev/service/PriceQuotationRequestService.java
+++ src/main/java/com/umss/dev/service/PriceQuotationRequestService.java
@@ -33,8 +33,8 @@ public class PriceQuotationRequestService {
 	}
 	
 	public Iterable<PriceQuotationOutput> getPriceQuotation(Integer idPriceQuotation){
-		
-		Optional<PriceQuotationRequest> request= priceQuotationRequestRepository.findById(idPriceQuotation);
+		Optional<SpendingUnitRequest> srequest= spendingUnitRequestRepository.findById(idPriceQuotation);
+		Optional<PriceQuotationRequest> request= priceQuotationRequestRepository.findById(srequest.get().getPriceQuotation().getIdPriceQuotationRequest());
 		return priceQuotationService.getPriceQuotationByOrder(request.get().getIdPriceQuotationRequest());
 	}
 	
