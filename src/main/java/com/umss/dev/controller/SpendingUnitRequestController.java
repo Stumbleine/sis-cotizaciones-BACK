2fc6b33b3dbdca7835feeee6737b15ae65a5588b
jh3ys0n <55772934+jh3ys0n@users.noreply.github.com>
2021-05-30 11:56:51 -0400

    Actualizando put para reporte

--- src/main/java/com/umss/dev/controller/SpendingUnitRequestController.java
+++ src/main/java/com/umss/dev/controller/SpendingUnitRequestController.java
@@ -101,7 +101,7 @@ public class SpendingUnitRequestController {
 			else {
 				if(request.getStatus().compareTo("Cotizando")==0) {
 					if(state.compareTo("Rechazado")==0 || state.compareTo("Aprobado")==0) {
-						
+						spendingUnitReqService.updateState(spendingUnitRequestId,state);
 						idPriceQuotation=spendingUnitReqService.getSpendingUnitRequestNormal(spendingUnitRequestId).getPriceQuotation().getIdPriceQuotationRequest();
 						idReport=reportService.createReport(idPriceQuotation,comentary);
 						documetQuotationService.createDocument(file,idReport);
