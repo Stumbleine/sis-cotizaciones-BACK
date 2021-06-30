0d53cfee6fc957ae98ab87b051494fd6613e5607
janeth <jl715643@gmail.com>
2021-06-30 03:39:29 -0400

    Arreglando

--- src/main/java/com/umss/dev/controller/SpendingUnitController.java
+++ src/main/java/com/umss/dev/controller/SpendingUnitController.java
@@ -1,21 +1,20 @@
 package com.umss.dev.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 import javax.annotation.security.PermitAll;
 
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.GetMapping;
+
 
 import com.umss.dev.entity.SpendingUnit;
 import com.umss.dev.output.CompleteSpendingUnitRequestOutput;
@@ -26,7 +25,6 @@ import com.umss.dev.service.SpendingUnitService;
 
 @RestController
 @CrossOrigin(origins = "http://localhost:4200", methods = {RequestMethod.GET, RequestMethod.POST})
-//@RequestMapping(value="/spendingUnit",  produces={"application/json"},method = RequestMethod.POST, consumes=MediaType.APPLICATION_JSON_VALUE)
 @RequestMapping("/spendingUnit")
 public class SpendingUnitController {
 
