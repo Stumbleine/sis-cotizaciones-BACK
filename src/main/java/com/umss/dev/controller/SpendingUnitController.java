92b559a0bbaad7940d8e2de9e1391e77b50fbe5a
janeth <jl715643@gmail.com>
2021-07-13 23:52:57 -0400

    Modificacion del tope

--- src/main/java/com/umss/dev/controller/SpendingUnitController.java
+++ src/main/java/com/umss/dev/controller/SpendingUnitController.java
@@ -16,6 +16,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 
@@ -85,8 +86,8 @@ public class SpendingUnitController {
 	}
 	
 	@PreAuthorize("hasRole('RAF')")
-	@PutMapping("/updateBudget/{budget}")
-	public ResponseEntity<?> setBudget(@PathVariable Double budget){
+	@PutMapping("/updateBudget")
+	public ResponseEntity<?> setBudget(@RequestParam("budget") Double budget){
 		return ResponseEntity.ok(spendingUnitService.setBudget(budget));
 	}
 
